a1435a700961ad2627abc9b61b355f41f4f32557
bo9us <142365240+bo9us@users.noreply.github.com>
2023-08-31 09:48:56 +0900

    Add files via upload

---
+++ remainder/remainder/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace remainder
+{
+    public class Solution
+    {
+        public int solution(int n)
+        {
+            for(int x = 2; x <= n; x++)
+            {
+                if(n % x == 1)
+                {
+                    return x;
+                }
+            }
+            return -1;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+            int result = solution.solution(10);
+            Console.WriteLine(result);
+        }
+    }
+}
