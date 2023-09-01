4952e0523a89022eccdd143615a40e5c0c9d0999
bo9us <142365240+bo9us@users.noreply.github.com>
2023-09-01 09:54:55 +0900

    Add files via upload

---
+++ list/list/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace list
+{
+    internal class Program
+    {
+        public static List<long> solution(int x, int n)
+        {
+            List<long> result = new List<long>();
+            for (int i = 1; i <= n; i++)
+            {
+                result.Add((long)x * i);
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            int x = 4;
+            int n = 3;
+            List<long> answer = solution(x, n);
+
+            Console.WriteLine("Result:");
+            foreach (long num in answer)
+            {
+                Console.Write(num + " ");
+            }
+        }
+    }
+}
