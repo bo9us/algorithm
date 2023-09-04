ea4dbfbca79db9bc80aa45d5cecd8ecbe63eb56d
bo9us <142365240+bo9us@users.noreply.github.com>
2023-09-04 10:48:58 +0900

    Add files via upload

---
+++ natural/natural/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace natural
+{
+    public class Solution
+    {
+        public int[] ReverseDigits(int n)
+        {
+            List<int> result = new List<int>();
+
+            if (n == 0)
+            {
+                result.Add(0);
+                return result.ToArray();
+            }
+
+            while (n > 0)
+            {
+                int digit = n % 10;
+                result.Add(digit);
+                n /= 10;
+            }
+
+            return result.ToArray();
+        }
+        
+        
+        public static void Main(string[] args)
+        {
+            int n = 12345;
+            Solution solution = new Solution();
+            int[] reverseDigits = solution.ReverseDigits(n);
+
+            Console.Write("[");
+            foreach (int digit in reverseDigits)
+            {
+                Console.Write(digit + ",");
+            }
+            Console.Write("]");
+        }
+    }
+}
