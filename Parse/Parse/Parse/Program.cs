1d457af624530bb2e6e9d0a41ee8363326f540ff
bo9us <rkdgmlrjs456@naver.com>
2023-09-18 10:19:29 +0900

    Parse

---
+++ Parse/Parse/Parse/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Parse
+{
+    public class Parse
+    {
+        public static void Main()
+        {
+            string str = "-12345";
+            int result;
+
+            if (int.TryParse(str, out result))
+            {
+                Console.WriteLine("정수로 변환된 값:  " + result);
+            }
+            else
+            {
+                Console.WriteLine("유효한 정수로 변환할 수 없습니다.");
+            }
+        }
+    }
+}
