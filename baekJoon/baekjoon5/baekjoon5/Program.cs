b42beab4f985056395bb0dd275359e84786731c6
bo9us <rkdgmlrjs456@naver.com>
2023-09-25 10:18:44 +0900

    baekJoon5

---
+++ baekJoon/baekjoon5/baekjoon5/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace baekjoon5
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string s = Console.ReadLine();
+            string[] strings = s.Split();
+
+            double a = double.Parse(strings[0]);
+            double b = double.Parse(strings[1]);
+
+            Console.WriteLine(a/b);
+        }
+    }
+}
