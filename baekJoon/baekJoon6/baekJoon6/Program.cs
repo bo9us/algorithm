d83dbb1211efb392821c64199e7fb5e693479106
bo9us <rkdgmlrjs456@naver.com>
2023-09-25 10:51:26 +0900

    baekJoon6

---
+++ baekJoon/baekJoon6/baekJoon6/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace baekJoon6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+            Console.WriteLine(int.Parse(s[0]) + int.Parse(s[1]));
+            Console.WriteLine(int.Parse(s[0]) - int.Parse(s[1]));
+            Console.WriteLine(int.Parse(s[0]) * int.Parse(s[1]));
+            Console.WriteLine(int.Parse(s[0]) / int.Parse(s[1]));
+            Console.WriteLine(int.Parse(s[0]) % int.Parse(s[1]));
+
+        }
+    }
+}
