f057b72640ae998cdc21a4267f7dd7fa401843e2
bo9us <rkdgmlrjs456@naver.com>
2023-09-26 09:56:41 +0900

    baekJoon7

---
+++ baekJoon/baekJoon7/baekJoon7/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace baekJoon7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string user_id;
+            user_id = Console.ReadLine();
+            Console.WriteLine(user_id + "??!");
+        }
+    }
+}
