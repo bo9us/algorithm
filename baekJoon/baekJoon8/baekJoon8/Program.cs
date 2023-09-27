b529dfad48e185b74f9a81e09ab5e13676212e8c
bo9us <rkdgmlrjs456@naver.com>
2023-09-27 09:49:50 +0900

    baekJoon8

---
+++ baekJoon/baekJoon8/baekJoon8/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace baekJoon8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            input = Console.ReadLine();
+            int i = int.Parse(input);
+            Console.WriteLine(i - 544);
+        }
+    }
+}
