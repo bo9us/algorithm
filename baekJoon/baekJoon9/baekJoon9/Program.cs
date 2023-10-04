7ecf42f90a3e470cdb18d0abe3ec000d9c94a717
bo9us <rkdgmlrjs456@naver.com>
2023-10-04 11:45:02 +0900

    baekJoon9

---
+++ baekJoon/baekJoon9/baekJoon9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+
+namespace baekJoon9
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = ReadLine().Split();
+            int a = int.Parse(s[0]);
+            int b = int.Parse(s[1]);
+            int c = int.Parse(s[2]);
+
+            WriteLine((a + b) % c);
+            WriteLine((a % c) + (b % c) % c);
+            WriteLine((a + b) % c);
+            WriteLine((a % c) + (b % c) % c);
+        }
+    }
+}
