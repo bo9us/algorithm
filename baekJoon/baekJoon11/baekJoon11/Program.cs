057043b0cabb3dae384a75bf4605e9419e229548
bo9us <rkdgmlrjs456@naver.com>
2023-10-16 09:41:14 +0900

    baekJoon11

---
+++ baekJoon/baekJoon11/baekJoon11/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace baekJoon11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+            long[] nums = new long[3];
+            long total = 0;
+
+            for(int i = 0; i < 3; i++)
+            {
+                nums[i] = long.Parse(s[i]);
+                total += nums[i];
+            }
+
+            Console.WriteLine(total);
+        }
+    }
+}
