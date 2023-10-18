f939b3a3825d1ed2db12b3799d7885e765db7e35
bo9us <rkdgmlrjs456@naver.com>
2023-10-18 09:59:06 +0900

    baekJoon12

---
+++ baekJoon/baekJoon12/baekJoon12/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace baekJoon12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\\    /\\");
+            Console.WriteLine(" )  ( ' ");
+            Console.WriteLine(" (  /  )");
+            Console.WriteLine(" \\(__)l");
+        }
+    }
+}
