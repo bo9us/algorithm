da5a3291a2026656c7f194fbe7e7e9acd6c808d7
bo9us <rkdgmlrjs456@naver.com>
2023-10-19 10:00:44 +0900

    baekJoon13

---
+++ baekJoon/baekJoon13/baekJoon13/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace baekJoon13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] in_data = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+            if (in_data[0] > in_data[1])
+            {
+                Console.WriteLine(">");
+            }
+            else if (in_data[0] < in_data[1])
+            {
+                Console.WriteLine("<");
+            }
+            else
+            {
+                Console.WriteLine("==");
+            }
+        }
+    }
+}
